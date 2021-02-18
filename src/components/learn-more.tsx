2c23e5f969f6e457396d55397464888b0409be1b
Moises Lopes Ferreira <moises.ferreira@bitfy.app>
2021-02-17 21:12:24 -0300

    Fix Link /home

--- src/components/learn-more.tsx
+++ src/components/learn-more.tsx
@@ -6,7 +6,7 @@ export default function LearnMore() {
 	return (
 		<div className={cn(styleUtils.appear, styleUtils['appear-fifth'], styles.learnMore)}>
 			Para inscrever suas obras, apresentações ou idéias{' '}
-			<a href={'/inscrições'} className={styles['learnMore-email']} rel="noopener noreferrer">
+			<a href={'/inscricoes'} className={styles['learnMore-email']} rel="noopener noreferrer">
 				clique aqui
 			</a>
 			.
