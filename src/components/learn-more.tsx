bb4b44e87cdd06825cb7e00ecfba983dd2f8e219
Moises Lopes Ferreira <moises.ferreira@bitfy.app>
2021-02-22 21:26:22 -0300

    Fix text

--- src/components/learn-more.tsx
+++ src/components/learn-more.tsx
@@ -4,12 +4,21 @@ import styles from './learn-more.module.css';
 
 export default function LearnMore() {
 	return (
-		<div className={cn(styleUtils.appear, styleUtils['appear-fifth'], styles.learnMore)}>
-			Para inscrever suas obras, apresentações ou idéias{' '}
-			<a href={'/inscricoes'} className={styles['learnMore-email']} rel="noopener noreferrer">
-				clique aqui
-			</a>
-			.
-		</div>
+		<>
+			<div className={cn(styleUtils.appear, styleUtils['appear-fifth'], styles.learnMore)}>			
+				Conheça o manifesto do{' '}
+				<a href={'/manifesto'} className={styles['learnMore-email']} rel="noopener noreferrer">
+					Festival Fruta Preta
+				</a>
+				.
+			</div>
+			<div className={cn(styleUtils.appear, styleUtils['appear-fifth'], styles.learnMore)}>
+				Para inscrever suas obras, apresentações ou idéias{' '}
+				<a href={'/inscricoes'} className={styles['learnMore-email']} rel="noopener noreferrer">
+					clique aqui
+				</a>
+				.
+			</div>
+		</>
 	);
 }
